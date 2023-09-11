85c423ce1ed6324c391f185a32e5c000fbb4b390
wokujr <wokujr@gmail.com>
2023-09-11 22:59:47 +0700

    saiki dadi?, gilani lah

--- frontend/src/app/api/axios.ts
+++ frontend/src/app/api/axios.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
+import * as process from "process";
 
 const instance = axios.create({
   // baseURL: process.env.REACT_APP_BACKEND_URL,
-  baseURL: "http://localhost:3000/api/v1",
+  // baseURL: "http://localhost:3000/api/v1",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
     withCredentials: true,
